859942cf57aafd75bb150ea14e97c22676209580
Kamil Samigullin <kamil@samigullin.info>
2022-06-22 11:53:32 +0300

    dev: add first test case for gopkg.in/yaml.v3

--- main_test.go
+++ main_test.go
@@ -6,13 +6,53 @@ import (
 	"os"
 	"testing"
 
-	yaml2 "github.com/goccy/go-yaml"
+	yaml1 "github.com/goccy/go-yaml"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	_ "gopkg.in/yaml.v3"
+	yaml2 "gopkg.in/yaml.v3"
 )
 
 func TestGoccyYamlParser(t *testing.T) {
+	f, err := os.Open("testdata/typical.yaml")
+	require.NoError(t, err)
+
+	raw, err := io.ReadAll(f)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+
+	t.Run("non-deterministic", func(t *testing.T) {
+		var props any
+		in := bytes.NewBuffer(raw)
+		dec := yaml1.NewDecoder(in)
+		assert.NoError(t, dec.Decode(&props))
+
+		out := bytes.NewBuffer(nil)
+		enc := yaml1.NewEncoder(out, yaml1.Indent(2))
+		assert.NoError(t, enc.Encode(props))
+		assert.NotEqual(t, string(raw), out.String())
+		// it should be equal, but has problems:
+		// - ordering
+		// - indent
+		// - empty vs null
+	})
+
+	t.Run("deterministic", func(t *testing.T) {
+		var props any
+		in := bytes.NewBuffer(raw)
+		dec := yaml1.NewDecoder(in, yaml1.UseOrderedMap())
+		assert.NoError(t, dec.Decode(&props))
+
+		out := bytes.NewBuffer(nil)
+		enc := yaml1.NewEncoder(out, yaml1.Indent(2))
+		assert.NoError(t, enc.Encode(props))
+		assert.NotEqual(t, string(raw), out.String())
+		// it should be equal, but has problems:
+		// - indent
+		// - empty vs null
+	})
+}
+
+func TestGopkgYamlParser(t *testing.T) {
 	f, err := os.Open("testdata/typical.yaml")
 	require.NoError(t, err)
 
@@ -27,27 +67,12 @@ func TestGoccyYamlParser(t *testing.T) {
 		assert.NoError(t, dec.Decode(&props))
 
 		out := bytes.NewBuffer(nil)
-		enc := yaml2.NewEncoder(out, yaml2.Indent(2))
+		enc := yaml2.NewEncoder(out)
+		enc.SetIndent(2)
 		assert.NoError(t, enc.Encode(props))
 		assert.NotEqual(t, string(raw), out.String())
-		// should be equal, but have problems:
+		// it should be equal, but has problems:
 		// - ordering
-		// - indent
-		// - empty vs null
-	})
-
-	t.Run("deterministic", func(t *testing.T) {
-		var props any
-		in := bytes.NewBuffer(raw)
-		dec := yaml2.NewDecoder(in, yaml2.UseOrderedMap())
-		assert.NoError(t, dec.Decode(&props))
-
-		out := bytes.NewBuffer(nil)
-		enc := yaml2.NewEncoder(out, yaml2.Indent(2))
-		assert.NoError(t, enc.Encode(props))
-		assert.NotEqual(t, string(raw), out.String())
-		// should be equal, but have problems:
-		// - indent
 		// - empty vs null
 	})
 }
